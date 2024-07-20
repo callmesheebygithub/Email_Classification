bb43647cc622393961441e3c8130bf8bc44a77b5
Muhammad Shoaib <mdshoaibhabib@gmail.com>
2024-07-20 10:15:52 -0700

    i added post method

--- Email_Classification/views.py
+++ Email_Classification/views.py
@@ -2,6 +2,12 @@ from django.http import HttpResponse as hr
 from django.shortcuts import render 
 
 def homepage(request):
+    try:
+        if request.method=="POST":
+            Email=request.POST.get("message")
+            return hr(Email)
+    except:
+        pass
     return render (request,"homepage.html")
 
 def about(request):
